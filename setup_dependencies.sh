c86aaf8e5252341277ece9119d6896cf33eb274e
Darius Marian <marian.darius98@gmail.com>
2022-03-25 02:13:45 +0200

    Upgrade dependencies.

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -6,19 +6,19 @@ mkdir -p .downloads
 rm -rf .downloads
 mkdir -p .downloads
 echo "Downloading libc++ source code..."
-wget -O .downloads/libcxx.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1/libcxx-10.0.1.src.tar.xz
+wget -O .downloads/libcxx.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/libcxx-14.0.0.src.tar.xz
 echo "Downloading libc++abi source code..."
-wget -O .downloads/libcxxabi.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1/libcxxabi-10.0.1.src.tar.xz
+wget -O .downloads/libcxxabi.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/libcxxabi-14.0.0.src.tar.xz
 
 echo "Decompressing libcxx into third_party directory..."
 rm -rf third_party/libcxx
 tar -xf .downloads/libcxx.tar.xz -C .downloads
-mv .downloads/libcxx-10.0.1.src third_party/libcxx
+mv .downloads/libcxx-14.0.0.src third_party/libcxx
 
 echo "Decompressing libcxx into third_party directory..."
 rm -rf third_party/libcxxabi
 tar -xf .downloads/libcxxabi.tar.xz -C .downloads
-mv .downloads/libcxxabi-10.0.1.src third_party/libcxxabi
+mv .downloads/libcxxabi-14.0.0.src third_party/libcxxabi
 
 echo "Removing temporary .downloads directory..."
 rm -rf .downloads
