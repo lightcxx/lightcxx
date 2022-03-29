569609f29faf7bb0eccdf67fdf4870b911847b37
Darius Marian <marian.darius98@gmail.com>
2022-03-29 20:09:52 +0300

    Implement [meta.trans.sign].

--- tests/utilities/meta/meta.trans/meta.trans.sign/make_signed.cpp
+++ tests/utilities/meta/meta.trans/meta.trans.sign/make_signed.cpp
@@ -1,1 +1,40 @@
-// REQUEST:SKIP "TODO Not tested"
+#include <type_traits>
+
+#include <cstdint>
+
+template<class T, class U>
+constexpr bool test_make_signed() {
+    static_assert(std::is_same_v<typename std::make_signed<T>::type, U>);
+    static_assert(std::is_same_v<std::make_signed_t<T>, U>);
+    return true;
+}
+
+static_assert(test_make_signed<char, signed char>());
+static_assert(test_make_signed<signed char, signed char>());
+static_assert(test_make_signed<unsigned char, signed char>());
+
+static_assert(test_make_signed<short, short>());
+static_assert(test_make_signed<unsigned short, short>());
+
+static_assert(test_make_signed<int, int>());
+static_assert(test_make_signed<unsigned int, int>());
+
+static_assert(test_make_signed<long, long>());
+static_assert(test_make_signed<unsigned long, long>());
+
+static_assert(test_make_signed<long long, long long>());
+static_assert(test_make_signed<unsigned long long, long long>());
+
+static_assert(test_make_signed<char8_t, int_least8_t>());
+static_assert(test_make_signed<char16_t, int_least16_t>());
+static_assert(test_make_signed<char32_t, int_least32_t>());
+static_assert(test_make_signed<wchar_t, int_least32_t>());
+
+enum E { a = 0 };
+static_assert(test_make_signed<E, int>());
+
+enum class EC { ac = 0 };
+static_assert(test_make_signed<EC, int>());
+
+enum class EC2 : unsigned short { ac2 = 0 };
+static_assert(test_make_signed<EC2, short>());
--- tests/utilities/meta/meta.trans/meta.trans.sign/make_unsigned.cpp
+++ tests/utilities/meta/meta.trans/meta.trans.sign/make_unsigned.cpp
@@ -1,1 +1,40 @@
-// REQUEST:SKIP "TODO Not tested"
+#include <type_traits>
+
+#include <cstdint>
+
+template<class T, class U>
+constexpr bool test_make_unsigned() {
+    static_assert(std::is_same_v<typename std::make_unsigned<T>::type, U>);
+    static_assert(std::is_same_v<std::make_unsigned_t<T>, U>);
+    return true;
+}
+
+static_assert(test_make_unsigned<char, unsigned char>());
+static_assert(test_make_unsigned<signed char, unsigned char>());
+static_assert(test_make_unsigned<unsigned char, unsigned char>());
+
+static_assert(test_make_unsigned<short, unsigned short>());
+static_assert(test_make_unsigned<unsigned short, unsigned short>());
+
+static_assert(test_make_unsigned<int, unsigned int>());
+static_assert(test_make_unsigned<unsigned int, unsigned int>());
+
+static_assert(test_make_unsigned<long, unsigned long>());
+static_assert(test_make_unsigned<unsigned long, unsigned long>());
+
+static_assert(test_make_unsigned<long long, unsigned long long>());
+static_assert(test_make_unsigned<unsigned long long, unsigned long long>());
+
+static_assert(test_make_unsigned<char8_t, uint_least8_t>());
+static_assert(test_make_unsigned<char16_t, uint_least16_t>());
+static_assert(test_make_unsigned<char32_t, uint_least32_t>());
+static_assert(test_make_unsigned<wchar_t, uint_least32_t>());
+
+enum E { a = 0 };
+static_assert(test_make_unsigned<E, unsigned int>());
+
+enum class EC { ac = 0 };
+static_assert(test_make_unsigned<EC, unsigned int>());
+
+enum class EC2 : unsigned short { ac2 = 0 };
+static_assert(test_make_unsigned<EC2, unsigned short>());
