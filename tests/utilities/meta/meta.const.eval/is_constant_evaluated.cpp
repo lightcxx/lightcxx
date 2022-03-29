110c7e63b988f0cad0971a33448bae7b782edfe9
Darius Marian <marian.darius98@gmail.com>
2022-03-30 00:28:38 +0300

    Implement [meta.const.eval].

--- tests/utilities/meta/meta.const.eval/is_constant_evaluated.cpp
+++ tests/utilities/meta/meta.const.eval/is_constant_evaluated.cpp
@@ -1,1 +1,26 @@
-// REQUEST:SKIP "TODO Not tested"
+#include <type_traits>
+
+#include "testing.h"
+
+#pragma clang diagnostic ignored "-Wconstant-evaluated"
+
+constexpr int f() {
+    if (std::is_constant_evaluated()) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+TEST() {
+    expect(!std::is_constant_evaluated());
+    static_assert(std::is_constant_evaluated());
+    static_assert(
+      std::is_same_v<std::bool_constant<std::is_constant_evaluated()>, std::bool_constant<true>>);
+
+    expect(f() == 2);
+    static_assert(f() == 1);
+
+    expect_is_noexcept(std::is_constant_evaluated());
+    expect_type(bool, std::is_constant_evaluated());
+}
